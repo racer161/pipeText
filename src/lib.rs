91cb5a272375f1b963743eb54bbcce400298a35a
Ethan Spurlock <ethan@spurlock.io>
2019-11-17 20:37:32 -0600

    broken but nearly ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 extern crate wasm_bindgen;
+#[macro_use]
+extern crate lazy_static;
 
 use wasm_bindgen::prelude::*;
+use lipschitz::incremental_diff::IncrementalDiff;
+use otcore::list::*;
+use lipschitz::lang::Parsable;
+
+mod recursive_tree_builder;
 
 #[wasm_bindgen]
 extern {
@@ -12,8 +19,31 @@ pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
 
-//TODO: implement pipetext.mjs here
-pub fn init_incremental_diff(source_code : &str)
+#[wasm_bindgen]
+pub struct Pipetext
 {
+    diff : IncrementalDiff,
+    doc_state : List<char>,
+    html_string : String
+}
+
+#[wasm_bindgen]
+impl Pipetext
+{
+    
+    pub fn from_string(source_code : &str) -> Pipetext
+    {
+        Pipetext
+        {
+            diff : IncrementalDiff::from_string(source_code, Parsable::Javascript),
+            doc_state : List::<char>::with_capacity(source_code.len()),
+            html_string : String::new()
+        }
+    }
+
+    pub fn refreshState() 
+    {
+
+    }
 
 }
